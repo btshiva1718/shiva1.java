0c0066e611eda04699a19720990ad4b69aebcc79
btshiva1718 <95751444+btshiva1718@users.noreply.github.com>
2022-11-29 17:22:44 +0530

    Update shiva1.java

--- shiva1.java
+++ shiva1.java
@@ -2,6 +2,6 @@
 
 class shiva1 {
     public static void main(String[] args) {
-        System.out.println("hello world my name is shiva boya talari git-jen"); 
+        System.out.println("hello world my name is shiva boya talari"); 
     }
 }
